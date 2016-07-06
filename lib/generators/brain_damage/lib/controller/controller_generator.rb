03939a7828817c563d3b4f944683a9942de60b04
Pedro Bernardes <ph@everywhere.com.br>
2016-07-06 17:05:46 -0300

    Extracted some controller_generator logic to ClassTemplateable parent

--- lib/generators/brain_damage/lib/controller/controller_generator.rb
+++ lib/generators/brain_damage/lib/controller/controller_generator.rb
@@ -1,32 +1,19 @@
-require_relative '../templateable/base'
+require_relative '../templateable/class_templateable'
 
 module BrainDamage
-  class ControllerGenerator < Templateable::Base
+  class ControllerGenerator < Templateable::ClassTemplateable
     attr_accessor :set_member_before_action_list
 
     def initialize(resource, options = {})
       @template_file = 'controller.rb'
-      @public_methods_templates = "#{dir}/templates/public_methods/*"
-      @private_methods_templates = "#{dir}/templates/private_methods/*"
-
       @set_member_before_action_list = [:show, :edit, :update, :destroy]
       super
     end
 
     def generate(controller_file_name)
       @controller_file_name = controller_file_name
-      @controller_code = File.read @controller_file_name if File.exists? @controller_file_name
-
-      extract_definitions
-      render
-    end
-
-    def public_methods
-      methods :public
-    end
-
-    def private_methods
-      methods :private
+      @current_code = File.read @controller_file_name if File.exists? @controller_file_name
+      super
     end
 
     def leading_class_method_calls
@@ -38,30 +25,6 @@ module BrainDamage
     end
 
     private
-    def methods(visibility)
-      definitions = {}
-
-      identation = if visibility == :public then 1 else 2 end
-
-      Dir[instance_variable_get("@#{visibility}_methods_templates")].map { |template_file|
-        method_code = render_erb_file(template_file).indent identation
-        method_name = RubySimpleParser::Method.extract_method_name method_code
-        definitions[method_name] = method_code
-      }
-
-      if @parser
-        @parser.send("#{visibility}_methods").each do |name, method|
-          definitions[name] = method.print
-        end
-      end
-
-      definitions.values.join("\n\n")[(identation*2)..-1]
-    end
-
-    def extract_definitions
-      @parser = RubySimpleParser::Parser.new @controller_code if @controller_code
-    end
-
     def dir
       __dir__
     end
---
+++ lib/generators/brain_damage/lib/templateable/class_templateable.rb
@@ -0,0 +1,54 @@
+require_relative 'base'
+
+module BrainDamage
+  module Templateable
+    class ClassTemplateable < Templateable::Base
+      def initialize(resource, options = {})
+        super
+        @public_methods_templates = "#{dir}/templates/public_methods/*"
+        @private_methods_templates = "#{dir}/templates/private_methods/*"
+      end
+
+      def public_methods
+        methods :public
+      end
+
+      def private_methods
+        methods :private
+      end
+
+      def generate(file_name)
+        extract_definitions
+        render
+      end
+
+      def leading_class_method_calls
+        ''
+      end
+
+      def methods(visibility)
+        definitions = {}
+
+        identation = if visibility == :public then 1 else 2 end
+
+        Dir[instance_variable_get("@#{visibility}_methods_templates")].map { |template_file|
+          method_code = render_erb_file(template_file).indent identation
+          method_name = RubySimpleParser::Method.extract_method_name method_code
+          definitions[method_name] = method_code
+        }
+
+        if @parser
+          @parser.send("#{visibility}_methods").each do |name, method|
+            definitions[name] = method.print
+          end
+        end
+
+        definitions.values.join("\n\n")[(identation*2)..-1]
+      end
+
+      def extract_definitions
+        @parser = RubySimpleParser::Parser.new @current_code if @current_code
+      end
+    end
+  end
+end
